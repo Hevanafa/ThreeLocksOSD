16acd20664a6991a190145818de0f61592154bb2
Hevanafa <dhevansafa4@gmail.com>
2026-09-04 10:45:12 +0700

    added display controls

--- unit1.pas
+++ unit1.pas
@@ -6,10 +6,19 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs,StdCtrls,ExtCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    CapsLockBox:TCheckBox;
+    NumLockBox:TCheckBox;
+    ScrollLockBox:TCheckBox;
+    PollTimer:TTimer;
+    procedure FormShow(Sender:TObject);
+    procedure PollTimerTimer(Sender:TObject);
   private
 
   public
@@ -22,6 +31,18 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.PollTimerTimer(Sender:TObject);
+begin
+
+end;
+
+procedure TForm1.FormShow(Sender:TObject);
+begin
+  PollTimer.Enabled := true;
+end;
 
 end.
 
