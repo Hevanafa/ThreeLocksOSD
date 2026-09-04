767baaf6ef37d6ed09ab25e4e50b0d81b34b478c
Hevanafa <dhevansafa4@gmail.com>
2026-09-04 11:19:59 +0700

    added init polling states

--- OSD.pas
+++ OSD.pas
@@ -5,8 +5,7 @@ unit OSD;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,ExtCtrls,
-  AppStates;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
 
 type
 
@@ -15,6 +14,8 @@ type
   TForm2 = class(TForm)
     StateLabel: TLabel;
     StatePollTimer: TTimer;
+    procedure FormCreate(Sender:TObject);
+    procedure FormShow(Sender:TObject);
     procedure StatePollTimerTimer(Sender:TObject);
   private
 
@@ -24,13 +25,36 @@ type
 
 implementation
 
+uses AppStates;
+
+var
+  lastNumLockState, lastCapsLockState, lastScrollLockState: boolean;
+  isFirstTime: boolean;
+
 {$R *.lfm}
 
 { TForm2 }
 
 procedure TForm2.StatePollTimerTimer(Sender:TObject);
 begin
+  if isFirstTime then begin
+    { Don't show any notifications yet }
+    lastNumLockState := numLockState;
+    lastCapsLockState := capsLockState;
+    lastScrollLockState := scrollLockState;
+  end;
 
+  { TODO: Process notifications }
+end;
+
+procedure TForm2.FormCreate(Sender:TObject);
+begin
+  isFirstTime := true;
+end;
+
+procedure TForm2.FormShow(Sender:TObject);
+begin
+  StatePollTimer.Enabled := true;
 end;
 
 end.
